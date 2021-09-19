9ded554a06294d1bd8c45eb64a579a38bc137740
Julian Erre <julian.erreguerena@gmail.com>
2021-09-19 20:46:43 -0300

    okaaa

--- root/views/stock.php
+++ root/views/stock.php
@@ -29,7 +29,7 @@
                 <li><a href="stock.php">Stock</a></li>
                 <li><a href="venta.php">Venta</a></li>
                 <li><a href="ingreso.php">Ingresar producto</a></li>
-                <li><a href="proveedores.php">Proveedores</a></li>
+                <li><a href="proveedores.php">Proveedore</a></li>
 
         </nav>
     </header>
